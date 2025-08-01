53219a60efb8a286319061bf0b4c983f7d7bf56b
JalenEvans <jalenemmanuelevans@gmail.com>
2025-08-01 14:37:29 -0400

    Removed Chat rooms

--- raypoint-backend/src/sockets/index.js
+++ raypoint-backend/src/sockets/index.js
@@ -1,21 +1,7 @@
 export default function handleSocket(io) {
     io.on('connection', socket => {
         console.log(`User connected: ${socket.id}`);
-
-        socket.on('join-room', room => {
-            socket.join(room);
-            console.log(`User ${socket.id} joined room: ${room}`);
-        });
-
-        socket.on('leave-room', room => {
-            socket.leave(room);
-            console.log(`User ${socket.id} left room: ${room}`);
-        });
-
-        socket.on('message', ({ room, data }) => {
-            io.to(room).emit('message', data);
-        });
-
+        
         socket.on('disconnect', () => {
             console.log(`User disconnected: ${socket.id}`);
         });
